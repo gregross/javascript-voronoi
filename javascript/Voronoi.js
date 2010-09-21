d86f27795627fd813740453a5bc650e407aac2b6
gregxross <gregxross@4166d7b7-e039-c47c-284d-62041379e80a>
2010-09-21 20:39:20 +0000

    added license block

--- javascript/Voronoi.js
+++ javascript/Voronoi.js
@@ -1,4 +1,26 @@
 /*
+
+Copyright by Greg Ross, 2010
+
+This file is part of JavaScript-Voronoi.
+
+JavaScript-Voronoi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+JavaScript-Voronoi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with JavaScript-Voronoi.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+/*
  * Register the name space
  * ***********************
  */
